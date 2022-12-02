763dd64ade0a44cb74cd82238b4a0caac6674b34
mmiller2001 <mmillermatute18@gmail.com>
2022-12-01 20:17:15 -0600

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Marlon Miller Matute on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
